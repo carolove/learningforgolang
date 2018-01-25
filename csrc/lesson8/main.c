c783e84703d49ae0259c1904b85aae028b980af0
陈丹 <dan.chen@mtime.com>
2018-01-25 15:57:40 +0800

    这是错误的因为尽管没释放是因为还未来得及栈覆盖而已不应该这么干对子函数应该全部做成纯函数的方式

--- csrc/lesson8/main.c
+++ csrc/lesson8/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char * getstackmemory(void){
+    char * str = "abcdefg";
+    return str;
+}
+
 int main(int argc, char **argv) {
     char * ch;
     ch = (char *)malloc(sizeof(char)*12);
@@ -8,5 +13,8 @@ int main(int argc, char **argv) {
     free(ch);
     ch = "this is world";
     printf("%d\n",ch[11]);
+
+    char *str = getstackmemory();
+    printf("%d\n",str[2]);
     return -1;
 }
