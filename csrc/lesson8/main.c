94729dd5f4f0f2d12475b520fa7b5709b95c24c8
陈丹 <dan.chen@mtime.com>
2018-01-25 16:11:29 +0800

    use const with pointer often

--- csrc/lesson8/main.c
+++ csrc/lesson8/main.c
@@ -7,15 +7,32 @@ char * getstackmemory(void){
     return str;
 }
 
-int main(int argc, char **argv) {
-    char * ch;
-    ch = (char *)malloc(sizeof(char)*12);
-    printf("%x\n", ch[11]);
-    free(ch);
-    ch = "this is world";
-    printf("%d\n",ch[11]);
+void dosomecalcatpurefunction( const char * from, char * to, int count) {
+    for (int i = 0; i < count; i ++) {
+        to[i] = from[i];
+    }
+    return;
+}
 
-    char *str = getstackmemory();
-    printf("%d\n",str[2]);
+void testconst(const char * str, char * const str1) {
+    *str1 = '1';
+    str++;
+    *str1= *str;
+    printf("%d\n", *str1);
+    return;
+}
+
+int main(int argc, char **argv) {
+    char * ch, *ch1;
+    int count  = 12;
+    ch = (char *)malloc(sizeof(char)*12);
+    for(int i = 0; i < count ; i++) ch[i] = i;
+    ch1 = (char *) malloc(sizeof(char)*12);
+    dosomecalcatpurefunction(ch, ch1, count);
+    for(int i = 0; i < count ; i++) printf("%d", ch1[i]);
+    printf("\n");
+    testconst(ch, ch1);
+    free(ch);
+    free(ch1);
     return -1;
 }
