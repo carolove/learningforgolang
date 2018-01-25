09c8b4ea7052c191cd72221d472b878af9590645
陈丹 <dan.chen@mtime.com>
2018-01-25 15:46:44 +0800

    malloc should compare with free

--- csrc/lesson8/main.c
+++ csrc/lesson8/main.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-    
+    char * ch;
+    ch = (char *)malloc(sizeof(char)*12);
+    printf("%x\n", &ch[0]);
+    free(ch);
+    ch = "this is world";
+    printf("%x\n",&ch[0]);
     return -1;
 }
