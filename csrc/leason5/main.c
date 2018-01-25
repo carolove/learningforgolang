796a5fac76112768d994f74f76388df42bc9acd3
陈丹 <dan.chen@mtime.com>
2018-01-25 14:19:57 +0800

    add str

--- csrc/leason5/main.c
+++ csrc/leason5/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char ** argv){
     char *str;
     str = (char *)malloc(sizeof(char)*6);
     printf("please input string\n");
     scanf("%s", str);
-    printf("string is %s", str);
+    printf("string is %s\n", str);
     free(str);
     return -1;
 }
