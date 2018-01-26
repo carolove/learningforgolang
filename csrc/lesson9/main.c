15983bbb2376ebbc55c47cab59172cc982222a20
陈丹 <chendan@www.mtime.com>
2018-01-26 16:29:38 +0800

    rewrite the malloc matrix

--- csrc/lesson9/main.c
+++ csrc/lesson9/main.c
@@ -6,59 +6,64 @@
 #define COL 4
 
 int main(int argc, char ** argv) {
-    int **Ma, **Mb;
-    int *Ha, *Hb;
+    int **ma, **mb;
+    int *ha, *hb;
     int i,j;
-    Ma = (int **) malloc(sizeof(int *) * ROW);
-    
-    if (Ma == NULL) {
-        printf("malloc failed~\n");
+    ma = (int **) malloc(sizeof(int *)* ROW);
+
+    if (ma == NULL ){
+        printf("malloc failed!\n");
         return -1;
     }
 
-    Mb = (int **) malloc(sizeof(int *) * COL);
-
-    if (Mb == NULL) {
-        free(Ma);
-        printf("malloc failed~\n");
+    mb = (int **) malloc(sizeof(int *) * COL);
+    if (mb == NULL) {
+        free(ma);
+        printf("malloc failed!\n");
         return -1;
     }
-    
-    for (i = 0 ; i < ROW; i ++ ) {
-        *(Ma + i) = (int *)malloc(sizeof(int)*COL);
+
+    for (i = 0; i < ROW; i ++ ){
+        *(ma + i) = (int *) malloc (sizeof(int) * COL);
     }
 
-    for (i = 0 ; i < COL; i ++ ) {
-        *(Mb + i) = (int *)malloc(sizeof(int)*ROW);
+    for (i = 0; i < COL; i ++ ) {
+        *(mb + i) = (int *) malloc(sizeof (int) * ROW);
     }
 
-    printf("input Matrix a's elements\n");
-    for (i = 0; i < ROW ; i ++ ) {
-        for (j = 0; j < COL; j ++) {
-            scanf("%d", &Ma[i][j]);
-        }
-    }
-
-    for (i = 0; i < ROW ; i ++ ) {
-        for (j = 0; j < COL; j ++) {
-            Mb[j][i] = Ma[i][j];
-        }
-    }
-
-    for (i = 0; i < COL ; i ++ ) {
-        for (j = 0; j < ROW; j ++) {
-            printf("%8d ", Mb[i][j]);
+    printf("please input matrix a!\n");
+    for (i = 0; i < ROW; i++) {
+        for (j = 0; j < COL ; j++) {
+            scanf("%d", &ma[i][j]); 
         }
         printf("\n");
     }
-    for (i = 0 ; i < ROW; i ++ ) {
-        free(Ma[i]);
+
+    for (i = 0; i < ROW; i++) {
+        for (j = 0; j < COL ; j++) {
+            mb[j][i] = ma[i][j]; 
+        }
     }
 
-    for (i = 0 ; i < COL; i ++ ) {
-        free(Mb[i]);
+    printf("output matrix b: \n");
+
+    for (i = 0; i < COL; i++) {
+        for (j = 0; j < ROW ; j++) {
+           printf("%8d ", mb[i][j]) ; 
+        }
+        printf("\n");
+    }   
+
+    for (i = 0; i < ROW; i ++ ){
+       free(ma[i]);
     }
-    free(Ma);
-    free(Mb);
+
+    for (i = 0; i < COL; i ++ ) {
+       free(mb[i]);
+    }
+
+    free(ma);
+    free(mb);
+
     return -1;
 }
