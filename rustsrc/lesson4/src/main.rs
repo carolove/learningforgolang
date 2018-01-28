4a2df06b4bb2cc1f025af76c164980dccc683397
陈丹 <chendan@chendandeMacBook-Pro.local>
2018-01-28 15:09:16 +0800

    remove some warning

--- rustsrc/lesson4/src/main.rs
+++ rustsrc/lesson4/src/main.rs
@@ -1,6 +1,10 @@
 fn main(){
     println!("hello world!" );
     exe(println);
+    println!("x, y's sum is  {}", sum(3, 4));
+    let sun_f : fn (i32, i32) -> i32 = sum_a;
+    println!("{}", sun_f(12,23)); 
+
 }
 
 fn exe(a : fn ()){
@@ -9,4 +13,12 @@ fn exe(a : fn ()){
 
 fn println(){
     println!("fn in println!" );
+}
+
+fn  sum(x: i32, y :i32) -> i32 {
+    return y + x;
+}
+
+fn sum_a(x: i32, y:i32) -> i32 {
+    x + y
 }
