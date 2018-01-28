dee8780d4894f0ef72af888bd244811879fbfbb6
陈丹 <chendan@chendandeMacBook-Pro.local>
2018-01-28 20:50:21 +0800

    mut struct

--- rustsrc/lesson6/src/main.rs
+++ rustsrc/lesson6/src/main.rs
@@ -1,8 +1,23 @@
 use std::sync::Arc;
 
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn  main(){
     let x = Arc::new(5);
     let y  = x.clone();
     println!("hello world!");
-    println!("x is {}, y is {}",x, y )
+    println!("x is {}, y is {}",x, y );
+
+    let mut p1 : Point = Point{x:5, y:6};
+
+    p1.x  = 15;
+    println!("p1.x is {}", p1.x);
+
+    let p2 : Point = Point{x:5, y:8};
+
+    println!("p2.y is {}", p2.y);
+
 }
