fad6e6f283939de47807dec2d8237aaa7741a6bf
chendan@cmcm.com <chendan@cmcm.com>
2018-03-09 15:48:06 +0800

    add something

--- gosrc/lesson11/main.go
+++ gosrc/lesson11/main.go
@@ -7,9 +7,10 @@ import (
 )
 
 func main() {
-	res, _ := fb.Get("/538744468", fb.Params{
+	res, err := fb.Get("/538744468", fb.Params{
 		"fields":       "first_name",
-		"access_token": "a-valid-access-token",
+		"access_token": "CAAWumgh06egBAOIW1QIJZCX7ZBdcZCX1Jj3grlBVipT8j0KAs8pEGYOKeSkgI7ZBZBlABizkquFGQkdHDDDeE58Bf8GkIu6LZCEzwE4K3HlSovXddEvvYdlHSoJ0ZAwQUELTX0Ip9EvCxTNSxMnPDJ1xjgKpFBu7KjYz7KQjJtdT90AqAOkca5Oh7mLJwLyveMurtaiZAzPhC0LZCE2qRcEuG",
 	})
-	fmt.Println("here is my facebook first name:", res["first_name"])
+
+	fmt.Println("here is my facebook first name:", res["first_name"], err)
 }
